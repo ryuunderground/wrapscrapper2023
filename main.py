faf254898399cce283798506c669959adaf210e7
ryuunderground <jaeking00001@gmail.com>
2023-07-25 14:53:55 +0900

    parameter,argument practice

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-def say_hello():
-    print("hello who r u?")
+def say_hello(user_name):
+    print("hello", user_name, "who r u?")
 
 
-def say_bye():
-    print("bye bye")
-    say_hello()
-# 파이썬은 빈칸으로 명령어 인식범위 인지
+say_hello("Ryu")
+say_hello("Louis")
+say_hello("Lauv")
 
+# def blah(papreter):
 
-say_bye()
-say_hello()
-say_bye()
+# blah("argument")
