3dba98437a50ff8c7cf90c1e1d0f8d414596207d
ryuunderground <jaeking00001@gmail.com>
2023-07-25 15:08:06 +0900

    argument recap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def say_hello(user_name, user_age):
-    print("hello", user_name, "who r u?")
-    print("R u", user_age, "years old?")
+def tax_calculator(bank_account):
+    print(bank_account * 0.35, "$")
 
 
-say_hello("Ryu", 25)
+tax_calculator(50000000)
+tax_calculator(5000)
