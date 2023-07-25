2cf8c5827bb5cec5a06e93a6a3a95984f28aee40
ryuunderground <jaeking00001@gmail.com>
2023-07-25 15:10:29 +0900

    Default parameters

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def tax_calculator(bank_account):
-    print(bank_account * 0.35, "$")
+def hello(name="Bitch"):
+    print("hello", name)
 
 
-tax_calculator(50000000)
-tax_calculator(5000)
+hello("Ryu")
+hello()
