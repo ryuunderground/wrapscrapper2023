2218f3ad8fcad1334f7172b30c314b7e87ba1b8c
ryuunderground <jaeking00001@gmail.com>
2023-07-25 21:11:08 +0900

    elif practice2

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ elif winner < 10:
     print("...At least u tried")
 else:
     print("That amount")
+
+# >= <=
+# != 같지 않음
+
+if winner != 10:
+    print("If")
+elif winner <= 25:
+    print("elif")
+elif winner == 50:
+    print("elif2")
+elif winner >= 75:
+    print("elif3")
+else:
+    print("else")
