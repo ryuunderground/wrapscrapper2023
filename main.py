bb304fffef00e4830e6240467186fbde0d65798d
ryuunderground <jaeking00001@gmail.com>
2023-07-25 20:54:47 +0900

    if practice

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-def juice_maker(fruit):
-    return f"{fruit} juice"
-
-
-def ice_maker(juice):
-    return f"cold {juice}"
-
-
-def sugar_maker(cold_juice):
-    return f"sweat {cold_juice}"
-
-
-def vending_machine(fruit="water..."):
-    juice = juice_maker(fruit)
-    cold_juice = ice_maker(juice)
-    product = sugar_maker(cold_juice)
-    print(product)
-
-
-flavor = "apple"
-vending_machine(flavor)
-vending_machine()
+# 조건문 양식
+# if condition:
+#    "write code run"
+a = 10
+b = 2
+c = 8
+if a == b + c:
+    print("Correct!")
