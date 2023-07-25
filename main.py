b8ec73bf4dd7b5f2da990997548642995a1883ed
ryuunderground <jaeking00001@gmail.com>
2023-07-25 15:16:11 +0900

    calc

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def hello(name="Bitch"):
-    print("hello", name)
+def plus(a=0, b=0):
+    print(a + b)
 
 
-hello("Ryu")
-hello()
+def times(a=0, b=0):
+    print(a * b)
+
+
+def divide(a=0, b=1):
+    print(a / b)
+
+
+def minus(a=0, b=0):
+    print(a - b)
+
+
+plus(100, 50)
+minus(100, 50)
+times(100, 50)
+divide(100, 50)
+
+plus()
+minus()
+divide()
+times()
