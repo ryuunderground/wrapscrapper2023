f6f5c2ef788fe0b2db707d13196f75160c6375cf
ryuunderground <jaeking00001@gmail.com>
2023-07-25 14:44:39 +0900

    def practice2

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def say_hello():
     print("hello who r u?")
 
 
+def say_bye():
+    print("bye bye")
+    say_hello()
+# 파이썬은 빈칸으로 명령어 인식범위 인지
+
+
+say_bye()
 say_hello()
-say_hello()
-say_hello()
-say_hello()
+say_bye()
