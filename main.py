82058df3da0c1eaa1bd859d2e08c9d26bf390432
ryuunderground <jaeking00001@gmail.com>
2023-07-25 20:57:56 +0900

    atm practice

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # 조건문 양식
 # if condition:
 #    "write code run"
-a = 10
-b = 2
-c = 8
-if a == b + c:
-    print("Correct!")
+# else:
+#    "write another code run"
+password_correct = True
+if password_correct:
+    print("Here is ur money")
+else:
+    print("Wrong password")
