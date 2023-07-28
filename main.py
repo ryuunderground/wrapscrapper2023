b0f27102161a6f3970193760ba603afc1e36d9c9
ryuunderground <jaeking00001@gmail.com>
2023-07-28 23:20:26 +0900

    method practice

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-days_of_week = ["Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"]
-
-name = "ryu"
-
-print(name.upper())
-print(name.capitalize())
-print(name.startswith("y"))
-print(name.endswith("u"))
-print(name.replace("u", "v"))
-print(name.isupper())
+days_of_week = ["Mon", "Tue", "Wed", "Thr", "Fri"]
 print(days_of_week)
+days_of_week.append("Sat")
+print(days_of_week)
+days_of_week.reverse()
+print(days_of_week)
+print(days_of_week.count("Mon"))
+days_of_week.remove("Mon")
+print(days_of_week)
+print(days_of_week[3])
+# 기계는 0부터 셈
+print(days_of_week.clear())
+print(days_of_week)
+insane = [1, 2, 3, "wanking", "weird", ["fuck", "me"]]
+insane.reverse()
+print(insane[0])
