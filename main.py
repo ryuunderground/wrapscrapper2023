fab2adbb3b2fa06ceaf5b6663717f52a63c8a980
ryuunderground <jaeking00001@gmail.com>
2023-07-28 23:27:32 +0900

    tuple practice

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-days_of_week = ["Mon", "Tue", "Wed", "Thr", "Fri"]
-print(days_of_week)
-days_of_week.append("Sat")
-print(days_of_week)
-days_of_week.reverse()
-print(days_of_week)
-print(days_of_week.count("Mon"))
-days_of_week.remove("Mon")
-print(days_of_week)
-print(days_of_week[3])
-# 기계는 0부터 셈
-print(days_of_week.clear())
-print(days_of_week)
-insane = [1, 2, 3, "wanking", "weird", ["fuck", "me"]]
-insane.reverse()
-print(insane[0])
+days = ("Mon", "Tue", "Wed")
+# 튜플은 변형 불가
+print(days[2])
+print(days[-1])
