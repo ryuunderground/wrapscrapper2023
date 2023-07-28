4e15fea1985f347019f87f0c9c9aec6a69669500
ryuunderground <jaeking00001@gmail.com>
2023-07-28 23:44:34 +0900

    dictionary practice

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-days = ("Mon", "Tue", "Wed")
-# 튜플은 변형 불가
-print(days[2])
-print(days[-1])
+player = {
+    'name': 'Ryu',
+    'age': 22,
+    'alive': True,
+    'fav_food': ["pizza", "korean"],
+    'disapear': 'soon'
+}
+print(player.get('age'))
+print(player['alive'])
+print(player.get('fav_food'))
+player.pop('disapear')
+print(player)
+player['xp'] = 250
+print(player)
+player['fav_food'].append("ramyeon")
+print(player['fav_food'])
