3790e47356e3f786ed06811e55602d7f5c2a1c7d
ryuunderground <jaeking00001@gmail.com>
2023-07-29 15:36:31 +0900

    for in practice

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-player = {
-    'name': 'Ryu',
-    'age': 22,
-    'alive': True,
-    'fav_food': ["pizza", "korean"],
-    'disappear': 'soon',
-    'best_song': ("Best of me", "Male fantasy", "Light switch"),
-    'friend': {
-        'name': 'Shin',
-        'age': 21,
-        'fav_food': ["pasta"],
-        'best_song': ("Save our lives", "Utopia", "Hijo dela luna"),
+websites = (
+    "google.com",
+    "instagram.com",
+    "youtube.com",
+    "airbnb.com",
+    "pornhub.com"
+)
 
-    }
-}
-# Player Update
-player['best_song'] = ["Microkosmos", "Make it right"]
-print(player)
-# Player Update 2
-player.pop('disappear')
-# Player Update 3
-player['friend']['fav_food'].append("creamy stuff")
-print(player)
+for website in websites:
+    print(website)
