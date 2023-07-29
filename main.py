38604d22f101d8895d5fd3f110638ebe2847f9f5
ryuunderground <jaeking00001@gmail.com>
2023-07-30 01:00:46 +0900

    bs rq install

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
+from bs4 import BeautifulSoup
+# pip install bs4
 import requests
-
-websites = (
-    "naver.com",
-    "https://google.com",
-    "https://daum.net",
-    "https://instagram.com",
-    "youtube.com"
-)
-results = {}
-
-for website in websites:
-    if not website.startswith("https://"):
-        website = f"https://{website}"
-    response = requests.get(website)
-    if response.status_code == 200:
-        results[website] = "OK"
-    else:
-        results[website] = "Failed"
-print(results)
+# pip install requests
