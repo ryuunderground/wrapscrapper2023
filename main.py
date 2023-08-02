d63a050c2e18cf939750198ba0a629580ced7931
ryuunderground <jaeking00001@gmail.com>
2023-08-02 20:18:25 +0900

    info extract

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 base_url = "https://www.jobkorea.co.kr/Search/?stext="
 search_term = "인턴"
 response = requests.get(f"{base_url}{search_term}")
-"""
+
 job_search = True
 while job_search:
     if response.status_code != 200:
@@ -15,20 +15,33 @@ while job_search:
         soup = BeautifulSoup(response.text, "html.parser")
         job_posts = soup.find_all('li', class_="list-post")
         for post in job_posts:
+            """회사 이름 따오기
+            post_list_corp = post.find_all(
+                'div', class_="post-list-corp")
+            for plc in post_list_corp:
+                for anchors in plc.find_all('a'):
+                    company = anchors.get('title')
+                    if company != "새창":
+                        companies = []
+                        companies.append(company)
+                        print(companies)"""
             post_list_info = post.find_all('div', class_="post-list-info")
             for pli in post_list_info:
-                # pli.pop(-1)
-                # ^마지막 요소 제거
                 for anchors in pli.find_all('a'):
                     anchor = anchors.get('href')
                     if not anchor.endswith('logpath=1') and not anchor.endswith('g7intern'):
                         links = []
                         links.append(anchor)
                         print(links)
-        job_search = False"""
+                    title = anchors.get('title')
+                    if title != None:
+                        titles = []
+                        titles.append(title)
+                for span in pli.find_all('span', class_="loc short"):
+                    location = str(span)
+                    locations = []
+                    locations.append(location[24:-7])
+                    print(titles, locations, links)
+                    print("////////////////////////")
 
-list_prac = [1, 2, 3]
-first, second, third = list_prac
-print(first, second, third)
-# 결과
-# 1 2 3
+        job_search = False
