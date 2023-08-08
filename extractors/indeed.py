ec2a345eeac3908c70f761a3ca3ef454c7bf51d5
ryuunderground <jaeking00001@gmail.com>
2023-08-08 18:02:38 +0900

    selenium base

--- extractors/indeed.py
+++ extractors/indeed.py
@@ -1,12 +1,9 @@
 from bs4 import BeautifulSoup
 # pip install bs4
-import requests
-# pip install requests
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 # 브라우저 꺼짐 방지 코드
@@ -15,17 +12,17 @@ browser = webdriver.Chrome(service=Service(
     ChromeDriverManager().install()), options=chrome_options)
 # 크롬드라이버를 최신으로 유지해줍니다
 
-driver = webdriver.chrome()
+driver = webdriver.Chrome()
 base_url_head = "https://kr.indeed.com/jobs?q="
-base_url_tail = "vjk=938c3c09748cc0cd"
+base_url_tail = "&vjk=938c3c09748cc0cd"
 search_keyword = "인턴"
 location_keyword = "서울"
 driver.get(
-    f"{base_url_head}{search_keyword}={location_keyword}{base_url_tail}")
+    f"{base_url_head}{search_keyword}&l={location_keyword}{base_url_tail}")
 
 # print(driver.page_source)
 
-if response.status_code != 200:
+if driver.status_code != 200:
     print("Can't request website")
 else:
     stack = 0
