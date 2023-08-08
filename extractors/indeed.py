ac1567c9fafcf8e83279080c6802ec5348d0f9a6
ryuunderground <jaeking00001@gmail.com>
2023-08-08 18:53:23 +0900

    selenium

--- extractors/indeed.py
+++ extractors/indeed.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 # 브라우저 꺼짐 방지 코드
@@ -17,15 +18,17 @@ base_url_head = "https://kr.indeed.com/jobs?q="
 base_url_tail = "&vjk=938c3c09748cc0cd"
 search_keyword = "인턴"
 location_keyword = "서울"
-driver.get(
+browser.get(
     f"{base_url_head}{search_keyword}&l={location_keyword}{base_url_tail}")
 
-# print(driver.page_source)
+print(browser.page_source)
 
-if driver.status_code != 200:
+"""if driver.status_code != 200:
     print("Can't request website")
 else:
     stack = 0
-    soup = BeautifulSoup(response.text, "html.parser")
+    soup = BeautifulSoup(driver.text, "html.parser")
     job_posts = soup.find_all('li')
-    print(len(job_posts))
+    print(len(job_posts))"""
+while (True):
+    pass
