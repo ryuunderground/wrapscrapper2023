62a353c8d0603e860f96f5212652147862d499cb
ryuunderground <jaeking00001@gmail.com>
2023-08-15 01:25:45 +0900

    import error

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-from bs4 import BeautifulSoup
-# pip install bs4
-import requests
-# pip install requests
 from extractors.jobkorea import extract_jobkorea_jobs
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from extractors.indeed import indeed_job_extract
 
-options = Options()
-options.add_argument("--no-snadbox")
-options.add_argument("--disable-dev-shm-usage")
+keyword, location = input("what are you looking for?").split()
+print(f"{keyword}and{location}")
+"""indeed = indeed_job_extract(keyword, location)
+jobkorea = extract_jobkorea_jobs(keyword)
 
-browser = webdriver.chrome(options=Options)
-
-jobs = extract_jobkorea_jobs("인턴")
-print(jobs)
+jobs = indeed + jobkorea
+for job in jobs:
+    print(job)
+    print("///////////\n//////")"""
