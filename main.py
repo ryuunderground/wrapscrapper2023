cecd8f2ab82529b7ce3248973063ead37e66b353
ryuunderground <jaeking00001@gmail.com>
2023-08-19 03:18:47 +0900

    render template flask

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
-from extractors.jobkorea import extract_jobkorea_jobs
-from extractors.indeed import indeed_job_extract
-from file import save_to_file
+from flask import Flask, render_template
 
-search_keyword, location_keyword = input("what are you looking for?").split()
-print(f"{search_keyword}and{location_keyword}")
+app = Flask("JobScrapper")
 
-indeed = indeed_job_extract(search_keyword, location_keyword)
-jobkorea = extract_jobkorea_jobs(search_keyword)
 
-jobs = indeed + jobkorea
-
-save_to_file(search_keyword, jobs)
-
-"""form flask import Flask
-app = Flask("JobScrapper)
 @app.route("/")
 def home():
-return "hey there!"
+    return render_template("home.html", name="Ryu")
 
-app.run("127.0.0.1")"""
+
+@app.route("/hello")
+def hello():
+    return "<h1>hello there</h1>"
+
+
+app.run("127.0.0.1")
